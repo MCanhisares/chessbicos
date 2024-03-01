112829ed15d153e90d851d7ba748549b9cef5566
Marcel Canhisares <marcel.canhisares@gmail.com>
2024-02-29 23:41:01 -0500

    Setting up gRPC server

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,36 @@
-mod server;
-
-use std::{
-    fs,
-    io::{BufRead, BufReader, Write},
-    net::{TcpListener, TcpStream},
-};
-
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-        handle_connection(stream)
-    }
+use tonic::{transport::Server, Request, Response, Status};
+pub mod chess {
+    tonic::include_proto!("chess"); // The string specified here must match the proto package name
 }
+use chess::match_server::{Match, MatchServer};
+use chess::{MoveRequest, MoveResponse};
 
-fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
-    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", "hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+#[derive(Debug, Default)]
+pub struct MatchService {}
+
+#[tonic::async_trait]
+impl Match for MatchService {
+  async fn move_piece(&self, request: Request<MoveRequest>) -> Result<Response<MoveResponse>, Status> {
+    println!("Got a request: {:?}", request);
+    let reply = chess::MoveResponse {
+      success: true,
+      board_state: "board_state".into()
     };
 
-    let contents = fs::read_to_string(filename).unwrap();
-    let length = contents.len();
+    Ok(Response::new(reply))
+  }
+}
 
-    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
-    stream.write_all(response.as_bytes()).unwrap();
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse()?;
+    let match_service = MatchService {};
+    
+    Server::builder()
+        .add_service(MatchServer::new(match_service))
+        .serve(addr)
+        .await?;
+    Ok(())
 }
